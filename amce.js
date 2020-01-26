7e11bac57083526fa4fad7d3f8d1e1561e954a85
peetklecha <54185879+peetklecha@users.noreply.github.com>
2020-01-26 11:34:26 -0500

    Update amce.js

--- amce.js
+++ amce.js
@@ -1,3 +1,5 @@
+
+//[pk] nice!
 const findEmployeeByName = (name, arr) => {
   return arr.find(elem => elem.name === name)
 }
@@ -5,7 +7,7 @@ const findEmployeeByName = (name, arr) => {
 
 
 
-
+//[pk] why return [] when there is no manager? undefined or null would be better
 const findManagerFor = (obj, arr) => {
   if (obj.managerId !== undefined) {
     return arr.find(elem => elem.id === obj.managerId)
@@ -13,10 +15,6 @@ const findManagerFor = (obj, arr) => {
     return []
   }
 }
-
-
-
-
 
 
 const findCoworkersFor = (obj, arr) => {
@@ -28,13 +26,18 @@ const findCoworkersFor = (obj, arr) => {
     }
   })
   return coworkers.slice(1)
+  //[pk] i see what you are trying to do but this is not the way to do it.
+  //[pk] you're sort of cheating because you are just exploiting the fact that you know that
+  //[pk] the first one happens to be the obj it gets called on. but that won't always be the case
+  //[pk] see solution for how to do it automatically
+  //[pk] also using the "filter" array method would be much less verbose
 }
 
 
 
 
 
-
+//[pk] adding arr[0] to the beginning is causing your output to not be right -- and again it's cheating like above!
 const findManagementChainForEmployee = (obj, arr) => {
   let newArr = []
   let upperManager = findManagerFor(obj, arr)
@@ -49,7 +52,7 @@ const findManagementChainForEmployee = (obj, arr) => {
 
 
 
-
+//[pk] good!
 const generateManagementTree = (employees) => {
   let tree = {}
   const manager = employees.find(employObj => employObj.managerId === undefined)
@@ -71,7 +74,7 @@ const generateManagementTree = (employees) => {
 
 
 
-
+//[pk] good but not generating the different numbers of dashes -- see solution
 const displayManagementTree = (obj) => {
   let resultStri = ''
   resultStri += obj.name + '\n'
